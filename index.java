894f349c0719f0afe11a84cafdfef8cc5962e902
Aditya-7747 <aj6051544@gmail.com>
2023-12-19 00:32:46 +0530

    Add Button

--- index.java
+++ index.java
@@ -2,7 +2,7 @@ public class index {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
 
-        System.out.println(" New Feature ");
+        System.out.println(" New Feature (Button)");
     }
 }  
 
