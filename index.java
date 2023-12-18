47a19f0bccd960cfea19e6d20dff2b248f3d9a0c
Aditya-7747 <aj6051544@gmail.com>
2023-12-19 00:54:04 +0530

    Add Dropdown

--- index.java
+++ index.java
@@ -1,12 +1,10 @@
 public class index {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-<<<<<<< HEAD
+
         System.out.println(" New Feature (Dropdown)");
     }
 }  
-=======
-       
-} 
 
->>>>>>> main
+
+
+
